a54caca8a3a865c4c516efb9cf4cf274c2dbc2a9
Christian Dupuis <cd@atomist.com>
2019-06-06 09:35:37 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http; 
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
